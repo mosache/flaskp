14e96d21c77188152563e2b245e40925da5e6d8b
mosache <330256579@qq.com>
2017-09-13 21:56:31 +0800

    init2

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database/temp.db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://vurtne:123456@localhost:3306/py_test?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'heheheheiehie'
--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
-from flask import render_template
+from flask import render_template, session,g,flash
 
 from app import app
 from app.forms import LoginForm
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
+@app.route('/login', methods=['POST'])
 def index():
     form = LoginForm()
+    if form.validate_on_submit():
+        g.user = {'username': form.name.data}
+        flash('Login success!!')
+        return render_template('main.html')
+    flash('has not login!!! please login!!!')
     return render_template('index.html', form=form)
